ebaea488dc305fe9aeef3ab0521fdb9f92462b32
savio591 <savio591@hotmail.com>
2021-05-09 12:43:15 -0300

    #19 - Copy popup

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,14 +1,35 @@
-import copyImg from "../assets/copy.svg";
-import popupArrowImg from "../assets/popupArrow.svg";
+import { BaseSyntheticEvent, ReactNode, SyntheticEvent } from "react";
 
-export default function Popup() {
-  return (
-    <div>
-      <div>
-        <img src={copyImg} alt="Copiar" />
-        <span>Copiar</span>
-      </div>
-      <img src={popupArrowImg} alt="" />
-    </div>
-  );
+import copy from "copy-text-to-clipboard";
+
+import copyImg from "../assets/copy.svg";
+import { PopupStyle } from "../styles/PopupStyle";
+import { Button } from "./Button";
+
+interface PopupProps {
+  type: "copy";
+  className?: string;
+  children: string;
+}
+
+
+export function Popup({ type, className, children }: PopupProps) {
+  function handleClickCopy() {
+    const textSplit = children.toString().split(" ")
+    const textToBeCopied = textSplit[1]
+    copy(textToBeCopied);
+    alert("Copiado: " + children);
+  }
+
+  // if (type === "copy") {
+    return (
+      <PopupStyle className={className}>
+        {children}
+        <div className="popupText">
+          <Button type="copy" data={copyImg} onClick={handleClickCopy} />
+          <span>Copiar</span>
+        </div>
+      </PopupStyle>
+    );
+  // }
 }
